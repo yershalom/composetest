a3c1e3cf2196711779618d2f952d169812c2227f
Shalom Yerushalmy <shyerush@microsoft.com>
2021-06-21 15:49:32 +0300

    adjustments

--- app1/app.py
+++ app1/app.py
@@ -6,6 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+
 def get_hit_count():
     retries = 5
     while True:
@@ -16,6 +17,7 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
+
 
 @app.route('/')
 def hello():
