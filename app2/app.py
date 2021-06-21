f4e851bc9af89697df39eeb84de45b95dd1c1717
Shalom Yerushalmy <shyerush@microsoft.com>
2021-06-21 18:24:09 +0300

    add logging

--- app2/app.py
+++ app2/app.py
@@ -1,13 +1,24 @@
 import requests
 from flask import Flask
 
+import logging
+from sys import stdout
+
+logger = logging.getLogger('app2')
+
+logger.setLevel(logging.INFO)
+logFormatter = logging.Formatter("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
+consoleHandler = logging.StreamHandler(stdout)
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
 app = Flask(__name__)
 
 
 def get_hit_count():
     resp = requests.get('http://app1.app1.svc.cluster.local:5000')
-    print('resp is: {}'.format(resp.text))
-    return resp.text
+    logger.info('resp is: {}'.format(resp.text))
+    return {'text': resp.text}
 
 
 @app.route('/')
